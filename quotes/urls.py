88ae69f33189b69d90b06632fe0c283be06b0d1d
Iury de Oliveira Gomes Figueiredo <ioliveira@id.uff.br>
2018-04-01 04:18:47 +0000

    Fixing urls.py.

--- quotes/urls.py
+++ quotes/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
